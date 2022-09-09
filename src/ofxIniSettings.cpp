546cb2bf4bf3a7304a540ee8289f67e6ef4782f5
Aashish Vasudevan <aashishvasu@gmail.com>
2022-09-09 18:10:28 +0530

    Added getters and setters for string, bool, int and float

--- src/ofxIniSettings.cpp
+++ src/ofxIniSettings.cpp
@@ -23,10 +23,52 @@ bool ofxIniSettings::load(string file)
 	return filepath.read(structure_);
 }
 
-bool ofxIniSettings::save(string file, bool prettyPrint)
+bool ofxIniSettings::save(string file, bool bPrettyPrint, bool bOverwrite)
 {
 	// Create a INIFile instance
 	const mINI::INIFile filepath(file);
 	
-	return filepath.generate(structure_, prettyPrint);
+	return bOverwrite ? filepath.generate(structure_, bPrettyPrint) : filepath.write(structure_, bPrettyPrint);
+}
+
+// Getters
+string ofxIniSettings::getString(const string section, const string key)
+{
+	return structure_.get(section).get(key);
+}
+
+bool ofxIniSettings::getBool(const string section, const string key)
+{
+	return ofToBool(getString(section, key));
+}
+
+int ofxIniSettings::getInt(const string section, const string key)
+{
+	return ofToInt(getString(section, key));
+}
+
+float ofxIniSettings::getFloat(const string section, const string key)
+{
+	return ofToFloat(getString(section, key));
+}
+
+// Setters
+void ofxIniSettings::setString(const string section, const string key, const string inString)
+{
+	structure_[section][key] = inString;
+}
+
+void ofxIniSettings::setBool(const string section, const string key, const bool inBool)
+{
+	setString(section, key, inBool ? "true" : "false");
+}
+
+void ofxIniSettings::setInt(const string section, const string key, const int inInt)
+{
+	setString(section, key, ofToString(inInt));
+}
+
+void ofxIniSettings::setFloat(const string section, const string key, const float inFloat)
+{
+	setString(section, key, ofToString(inFloat));
 }
--- src/ofxIniSettings.h
+++ src/ofxIniSettings.h
@@ -8,7 +8,7 @@
 # pragma once
 
 #include "ofMain.h"
-#include <string.h>
+#include <string>
 #if (_MSC_VER)
 #include "../libs/mini/ini.h"
 #else
@@ -30,8 +30,11 @@ using namespace std;
 class ofxIniSettings
 {
 public:
+	// Constructor: ofxIniSettings
 	ofxIniSettings();
+	// Destructor: ~ofxIniSettings
 	~ofxIniSettings();
+	// Group: File IO
 	/**
 	 * Function: load
 	 * Load an INI structure into memory to access it. If no file exists, ini structure will be empty.
@@ -52,7 +55,8 @@ public:
 	 *
 	 * Parameters:
 	 * file - The file path to the .ini file. Relative to the executable directory.
-	 * prettyPrint - Whether white spaces should be added (true) or ignored (false) in the output file.
+	 * bPrettyPrint - Whether white spaces should be added (true) or ignored (false) in the output file. Defaults to true.
+	 * bOverwrite - Whether the file should be overwritten on save, or appended. Defaults to false.
 	 *
 	 * Returns:
 	 * True if file was successfully saved.
@@ -60,12 +64,84 @@ public:
 	 * See also:
 	 * <structure_>
 	 */
-	bool save(string file = DEFAULT_SETTINGS_FILE, bool prettyPrint = true);
+	bool save(string file = DEFAULT_SETTINGS_FILE, bool bPrettyPrint = true, bool bOverwrite = false);
+
+	// Group: Data getters
+	// All data getters call <getString> and then convert to their various types and have the same function parameters. Do note that this is not a safe
+	// operation and will result in empty return values if the keys don't exist in the ini.
+	//
+	// Parameters:
+	// string section - The name of the section in the ini file.
+	// string key - The name of the key in the section above in the ini file.
+	/**
+	 * Function: getString
+	 *
+	 * Returns:
+	 * String value at the current section and key in the ini.
+	 */
+	string getString(string section, string key);
+	/**
+	 * Function: getBool
+	 *
+	 * Returns:
+	 * Boolean value at the current section and key in the ini.
+	 */
+	bool getBool(string section, string key);
+	/**
+	 * Function: getInt
+	 *
+	 * Returns:
+	 * Integer value at the current section and key in the ini.
+	 */
+	int getInt(string section, string key);
+	/**
+	 * Function: getFloat
+	 *
+	 * Returns:
+	 * Floating point value at the current section and key in the ini.
+	 */
+	float getFloat(string section, string key);
+
+	// Group: Data setters
+	// All data setters call <setString> at the end after their data types have been converted to string.
+	//
+	// Parameters:
+	// string section - The name of the section in the ini file.
+	// string key - The name of the key in the section above in the ini file.
+	/**
+	 * Function: setString
+	 *
+	 * Parameters:
+	 * inString - Reference to the input string.
+	 */
+	void setString(string section, string key, string inString);
+	/**
+	 * Function: setBool
+	 *
+	 * Parameters:
+	 * inBool - Reference to the input bool.
+	 */
+	void setBool(string section, string key, bool inBool);
+	/**
+	 * Function: setInt
+	 *
+	 * Parameters:
+	 * inInt - Reference to the input bool.
+	 */
+	void setInt(string section, string key, int inInt);
+	/**
+	 * Function: setFloat
+	 *
+	 * Parameters:
+	 * inFloat - Reference to the input float.
+	 */
+	void setFloat(string section, string key, float inFloat);
 	
 private:
+	// Group: Variables
 	/**
 	 * Variable: structure_
-	 * This ini structure will store all the values needed by the ini file 
+	 * This ini structure will store all the values needed by the ini file.
 	 */
 	mINI::INIStructure structure_;
 };
